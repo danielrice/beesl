d8b15acd2bfa7bcc3906fa967150bae0d0eb5c6a
daze <divadz3@yahoo.com>
2010-11-15 20:13:08 -0500

    -Modified layouts (oddly, syntax highlighting goofs now...) -jquery.rails.js created -in gradual process to UJS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
-  def show_page # called from link to remote in sections/show by sidebar element
+  def show_page # called from link to remote in sections/show by sidebar element...REMOVE
     @page = Page.find(params[:id])
     render :partial => "show", :locals => { :page => @page }
   end
