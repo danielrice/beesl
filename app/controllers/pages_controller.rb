86df17faf924d9d426e64744c55ff89f05389914
David Zhang <divadz3@yahoo.com>
2011-01-07 00:25:32 -0500

    Breadcrumbs created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,13 @@ class PagesController < ApplicationController
 		respond_to do |format|
 			format.js {
 				render :update do |page| # uses Prototype, the JS framework
+					# updates breadcrumbs
+					page.replace_html 'breadcrumb_page', :partial => 'layouts/breadcrumb_page', :locals => { :page_id => @page.id }
+					
+					# updates selected page in sidebar
 					page.replace_html 'sidebar_area', :partial => 'sections/sidebar', :locals => { :section => @page.section, :page => @page }
+					
+					# updates 'show page' area
 					page.replace_html 'show_page', :partial => 'show_page', :locals => { :page => @page }
 				end
 			}
