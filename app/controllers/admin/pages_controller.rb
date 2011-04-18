03fdf7922e2cd0f88b83f04e4ecc78de8c30116a
David Zhang <divadz3@yahoo.com>
2011-04-18 16:35:06 -0400

    general changes

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -41,7 +41,7 @@ class Admin::PagesController < ApplicationController
 
     respond_to do |format|
       if @page.save
-        format.html { redirect_to(admin_pages_url, :notice => 'Page was successfully created.') }
+        format.html { redirect_to(admin_pages_path, :notice => 'Page was successfully created.') }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @page.errors, :status => :unprocessable_entity }
@@ -49,14 +49,12 @@ class Admin::PagesController < ApplicationController
     end
   end
 
-  # PUT /pages/1
-  # PUT /pages/1.xml
   def update
     @page = Page.find(params[:id])
 
     respond_to do |format|
       if @page.update_attributes(params[:page])
-        format.html { redirect_to(admin_pages_url, :notice => 'Page was successfully updated.') }
+        format.html { redirect_to(admin_pages_path, :notice => 'Page was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -65,14 +63,12 @@ class Admin::PagesController < ApplicationController
     end
   end
 
-  # DELETE /pages/1
-  # DELETE /pages/1.xml
   def destroy
     @page = Page.find(params[:id])
     @page.destroy
 
     respond_to do |format|
-      format.html { redirect_to(admin_pages_url) }
+      format.html { redirect_to(admin_pages_path) }
       format.xml  { head :ok }
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@
 User.create(:login=>"dakota",:password=>"northandsouth",:password_confirmation=>"northandsouth",:email=>"dmonopoly10@gmail.com")
 
 # # Creating the sections
-%w[ Education Research Facilities Magic Temporary  ].each do |section_name|
-	Section.find_or_create_by_name(section_name)
+[ 'Education', 'Research', 'Faculty & Members', 'Advisory Board', 'Financial Assistance', 'Program Sponsors', 'Services'  ].each do |section_name|
+	Section.find_or_create_by_name(section_name, :has_sidebar => true)
 end
 puts "---found or created sections"
 
@@ -17,7 +17,7 @@ if Page.count == 0 # to prevent duplicate articles if rake db:seed is called > o
 =end
 
 	Section.all.each { |s|
-		5.times do Factory.create(:page, :section => s) end
+		2.times do Factory.create(:page, :section => s) end
 	}
 	puts "---created pages"
 
@@ -39,7 +39,7 @@ Power generation and distribution
 The Energy and Indoor Environmental Systems Program at Syracuse University is actively engaged in teaching, research and outreach. Its vision is to be a leading student-centered teaching and research program that facilitates high quality learning, and is highly responsive to the needs of relevant industries.")
 =end
 	Page.all.each { |p|
-    5.times do Factory.create(:panel, :page => p) end
+    3.times do Factory.create(:panel, :page => p) end
   }
   puts "---created panels"
 else
