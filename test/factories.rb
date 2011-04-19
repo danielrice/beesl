2845f2ff7d4628574331e482b118aae09839df54
David Zhang <dmonopoly10@gmail.com>
2011-04-19 19:07:25 -0400

    factories.rb - removed _

--- test/factories.rb
+++ test/factories.rb
@@ -1,11 +1,11 @@
 Factory.define :page do |t|
 	t.association :section # belongs to a section
-	t.name { |x| "#{x.section.name}_Page#{Factory.next(:count)}" }
+	t.name { |x| "#{x.section.name} Page#{Factory.next(:count)}" }
 end
 
 Factory.define :panel do |t|
   t.association :page
-  t.title { |x| "#{x.page.name}_Panel#{Factory.next(:count)}" }
+  t.title { |x| "#{x.page.name} Panel#{Factory.next(:count)}" }
   t.content { Forgery::LoremIpsum.paragraphs(5) }
 end
 
